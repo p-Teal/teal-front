e0707ec63422c2c6b16e609b0ea0ee0824ad63d3
folows1 <michelribeirocorrea187@gmail.com>
2023-03-21 18:04:05 -0300

    register

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ function App() {
   
 
   return (
-    <Login />
-    // <CadastroVoluntario />
+    // <Login />
+    <CadastroVoluntario />
   )
 }
 
--- src/pages/CadastroVoluntario.tsx
+++ src/pages/CadastroVoluntario.tsx
@@ -1,39 +1,33 @@
-import { UserCirclePlus } from "@phosphor-icons/react";
+import { GithubLogo, Link, UserCirclePlus } from "@phosphor-icons/react";
 import FormInputVoluntario from "../components/FormInputVoluntario";
 
 
 export default function CadastroVoluntario() {
   return (
-    <form>
-      <div className="flex flex-col min-h-screen">
-        <div className="flex flex-col bg-white h-fit max-h-[600px] items-center p-8 w-fit mx-auto my-auto rounded hover:shadow-xl border-y-4 border-teal-500">
-          <h1 className="text-3xl text-center mb-8 text-slate-700">
-            Cadastro de Voluntário
-          </h1>
-          <div className="w-full overflow-y-scroll p-1">
-            
-            <FormInputVoluntario type="text" name="nome" text="Nome" />
-            <FormInputVoluntario type="email" name="email" text="Email" autocomplete />
-            <FormInputVoluntario type="text" name="cpf" text="CPF" />
-            <FormInputVoluntario type="text" name="telefone" text="Telefone" />
-            <div className="h-2 w-full bg-teal-500 rounded mb-4"></div>
-            <h2 className="text-xl text-center mb-8 text-slate-700">
-              Informações de Login
-            </h2>
-            <FormInputVoluntario type="text" name="login" text="Chave para Login" />
-            <FormInputVoluntario type="password" name="senha" text="Senha" />
-            
-          </div>
-
-          <div className="mt-8 bottom-0 w-full">
-          <button className="bg-teal-500 hover:bg-teal-700 text-white font-bold p-4 rounded my-6 shadow-md flex flex-row justify-center items-center w-full">
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <div className="rounded hover:shadow-xl bg-white p-8 mt-7 max-w-sm border-y-4 border-teal-500">
+        <div className="w-full mb-8">
+          <img src="/src/assets/Logo.svg" alt="Logo" className="mx-auto" />
+        </div>
+        <h1 className="text-4xl text-center mb-8 text-slate-700">Cadastrar Voluntário</h1>
+        <form className="flex flex-col">
+          <FormInputVoluntario type="text" name="nome" text="Nome Completo"/>
+          <FormInputVoluntario type="text" name="cpf" text="Cpf"/>
+          <FormInputVoluntario type="text" name="celular" text="Celular"/>
+          <FormInputVoluntario type="password" name="senha" text="Senha"/>
+          <button className="bg-teal-500 hover:bg-teal-700 text-white font-bold p-4 rounded my-6 shadow-md flex flex-row justify-center items-center">
             <UserCirclePlus size={32} />
-            Cadastrar
+            Registrar
           </button>
-          </div>
-
+        </form>
+        <div className="text-right">
+          <a href="#" className="text-slate-500 hover:text-slate-700 text-sm">Já possuí cadastro? Logar</a>
         </div>
       </div>
-    </form>
+      <div className="mb-2 mt-5 flex flex-row">
+        <GithubLogo size={32} color="#334155" className="cursor-pointer mr-2"/>
+        <Link size={32} color="#334155" className="cursor-pointer ml-2"/>
+      </div>
+    </div>
   )
 }
