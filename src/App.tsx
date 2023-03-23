605bec414169f10a5479d5e0c2ab88afe992023a
folows1 <michelribeirocorrea187@gmail.com>
2023-03-23 18:02:44 -0300

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
 import CadastroVoluntario from "./pages/CadastroVoluntario";
 import Login from "./pages/Login";
 
@@ -10,9 +11,13 @@ function App() {
       <Routes>
         <Route
           path="/"
-          element={<h1>e</h1>}>
+          element={<Layout />}>
             <Route index element={<h1>index</h1>} />
             <Route path="voluntarios" element={<h1>voluntarios</h1>} />
+            <Route path="animais" element={<h1>animais</h1>} />
+            <Route path="adocoes" element={<h1>adocoes</h1>} />
+            <Route path="adotantes" element={<h1>adotantes</h1>} />
+            <Route path="sobre" element={<h1>sobre</h1>} />
           </Route>
         <Route path="/login" element={<Login />} />
         <Route path="/cadastro" element={<CadastroVoluntario />} />
---
+++ src/components/Layout.tsx
@@ -0,0 +1,11 @@
+import { Outlet } from "react-router-dom";
+
+export default function Layout () { 
+
+  return (
+    <div>
+      <h1>Layout</h1>
+      <Outlet />
+    </div>
+  )
+}
